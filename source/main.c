1d344f9b09cc40e3be5794512e2ca95e96df376b
Stjepan Poljak <stjepan@seahost.localdomain>
2018-10-21 16:05:39 +0200

    Removed buffer transform prototype.

--- source/main.c
+++ source/main.c
@@ -10,32 +10,5 @@ int main (int argc, char *argv[]) {
 
     MDGUI__deinit (&mdgui);
 
-    return 0;
-}
-
-void transform (volatile MD__buffer_chunk_t *curr_chunk,
-                unsigned int sample_rate,
-                unsigned int channels,
-                unsigned int bps) {
-
-    for (int i=0; i<curr_chunk->size/((bps/8)*channels); i++) {
-
-        for (int c=0; c<channels; c++) {
-
-            // this will depend on bps...
-            short data = 0;
-
-            for (int b=0; b<bps/8; b++) {
-
-                data = data + ((short)(curr_chunk->chunk[i*channels*(bps/8)+(c*channels)+b])<<(8*b));
-            }
-
-            // transform
-
-            for (int b=0; b<bps/8; b++) {
-
-                curr_chunk->chunk[i*channels*(bps/8)+(c*channels)+b] = data >> 8*b;
-            }
-        }
-    }
+    return EXIT_SUCCESS;
 }
